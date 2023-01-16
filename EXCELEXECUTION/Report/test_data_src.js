32d5cd53b20ddf8a27ad8bc6bc39bbb26282b49f
Semirarapoc <ccdesphy@semirarampc.com>
2023-01-16 15:29:35 +0800

    uft

---
+++ EXCELEXECUTION/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\ccdesphy\\Documents\\UFT16\\EXCELEXECUTION_MF',namePairs:[]}]};
---
+++ EXCELEXECUTION/Res4/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\ccdesphy\\Documents\\UFT_Jenk\\EXCELEXECUTION_MF\\Res4',namePairs:[]}]};
